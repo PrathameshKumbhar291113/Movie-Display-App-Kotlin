4c0390d7e8eac2caf94ea69781c069aa9db2e6d0
Rishabh-Invizio <rishabh.more@inviziosolutions.com>
2022-07-21 15:44:40 +0530

    [API Setup]: Steps to setup the movies api for our app 1. Added splitties to our app 2. Added base url and api-key to our Constants.kt 3. Added Internet permission to AndroidManifest.xml 4. Created interface for our Movies Api. 5. Created Singleton for Okhttp client and Retrofit 6. Updated Home Activity to call movies api when screen is loaded & display items to list

---
+++ app/src/main/java/com/example/moviedisplayapp/api/ApiService.kt
@@ -0,0 +1,36 @@
+package com.example.moviedisplayapp.api
+
+import com.example.moviedisplayapp.BuildConfig
+import com.example.moviedisplayapp.Constants
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiService {
+
+    val client = OkHttpClient.Builder().apply {
+        if(BuildConfig.DEBUG) {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            addInterceptor(loggingInterceptor)
+        }
+        readTimeout(1, TimeUnit.MINUTES)
+        connectTimeout(30, TimeUnit.SECONDS)
+        writeTimeout(1, TimeUnit.MINUTES)
+    }.build()
+
+    val restService = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(
+            MoshiConverterFactory.create(
+                Moshi.Builder()
+                    .addLast(KotlinJsonAdapterFactory())
+                    .build()
+            )
+        ).build()
+        .create(MoviesApi::class.java)
+}
